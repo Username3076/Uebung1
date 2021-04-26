d8f5a3b63e5d29ab83c99e79aaafcfa7a0056c22
Username3076 <luca.he.14.5@gmail.com>
2021-04-26 20:57:33 +0200

    test

---
+++ calculator.js
@@ -0,0 +1,20 @@
+import route from route.js
+
+var sum;
+var Array;
+function calculateNumberCoordinates(array){
+
+    sum = 0;
+
+    for(var i = 0; i < array.length-1;i++){
+        sum += 1;
+    }
+
+    var Array = [sum];
+}
+
+
+
+function calculatePointInPolygon(point, polygon){
+
+}
